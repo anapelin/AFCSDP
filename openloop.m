0177bb9f97170f8b4c4bc34f35b61ef01e8dfc3d
CristianPetria <118188223+CristianPetria@users.noreply.github.com>
2025-01-10 11:54:05 +0100

    Add files via upload

--- openloop.m
+++ openloop.m
@@ -12,16 +12,17 @@
 %================================================
 clear;
 
-global fi_flag_Simulink
+global fi_flag_Simulinkn
 
 newline = sprintf('\n');
+close all
 
 rng(1,"twister");
 
 %% Trim aircraft to desired altitude and velocity
 %%
-altitude = 15000;
-velocity = 500;
+altitude = 40000;
+velocity = 300;
 
 FC_flag = 1; % Trim for steady wings-level flight
 
@@ -71,7 +72,7 @@ state_names = {
 };
 %% Decoupled states
 %Vt,α,θ,q, de, dt -> 7, 8, 5, 11, 14, 13
-%β,ϕ,p,r,da, dr -> 9, 4, 10, 12, 15, 16
+%β,ϕ,p,r,da,dr -> 9, 4, 10, 12, 15, 16
 
 long_indices_states = [7 8 5 11 14 13];
 long_indices_states_wo = [1 2 3 4];
@@ -160,9 +161,288 @@ sys_lat = ss(A_lat, B_lat, C_lat,D_lat);
 % TODO: replace indexes of [V, alpha, theta, q] and [beta, phi, p, r]
 % TODO: Plot the different eigenmotion time responses to support the numbers calculated above
 
-% Short period response 
-figure
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Longitudinal Eigenmotions
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+C_ac_long = C_long([1 2], [1 2 3 4]);
+D_ac_long = zeros(2, 2);
+
+% Full longitudinal state-space model (4 states: Vt, alpha, theta, q)
+sys_long_full = ss(A_ac_long, B_ac_long, C_ac_long, D_ac_long);
+
+% Get the eigenvector corresponding to the short-period mode
+[long_eigvecs, ~] = eig(A_ac_long);
+%[num_states, ~] = size(sys_long_full.A);
+%disp(['Number of states: ', num2str(num_states)]);
+
+%%%%%%%%%%%%%%%
+%% Short Period
+%%%%%%%%%%%%%%%
+
+% Choose the eigenvector corresponding to the short-period mode
+sp_init_cond = real(long_eigvecs(:, 1)); % Short-period eigenvector (real part)
+%sp_init_cond = [0,0,0,0];
+
+% Create the time vector for simulation
+t = 0:0.01:10;
+
+% Define input as zero for the short period motion (no external input)
+u = zeros(length(t), 2); % Two input channels for elevator and throttle 
+u(:, 1) = -1; % Negative elevator step input
+
+% Simulate the response of the system using lsim
+[y_sp, t_out, x_sp] = lsim(sys_long_full, u, t, sp_init_cond);
+
+% Plot V_t (Speed)
+figure;
+plot(t_out, x_sp(:, 1), 'b', 'LineWidth', 1.5);
+title('Short Period Time Response: V_t (Speed)');
+xlabel('Time (s)');
+ylabel('V_t');
+grid on;
+
+% Plot \alpha (Angle of Attack)
+figure;
+plot(t_out, x_sp(:, 2), 'r', 'LineWidth', 1.5);
+title('Short Period Time Response: \alpha (Angle of Attack)');
+xlabel('Time (s)');
+ylabel('\alpha (deg)');
+grid on;
+
+% Plot \theta (Pitch Angle)
+figure;
+plot(t_out, x_sp(:, 3), 'g', 'LineWidth', 1.5);
+title('Short Period Time Response: \theta (Pitch Angle)');
+xlabel('Time (s)');
+ylabel('\theta (deg)');
+grid on;
+
+% Plot q (Pitch Rate)
+figure;
+plot(t_out, x_sp(:, 4), 'm', 'LineWidth', 1.5);
+title('Short Period Time Response: q (Pitch Rate)');
+xlabel('Time (s)');
+ylabel('q (deg/s)');
+grid on;
+
+%%%%%%%%%%
+%% Phugoid
+%%%%%%%%%%
+
+ph_init_cond = real(long_eigvecs(:, 3));
+%ph_init_cond = [0,0,0,0];
+
+
+% Create the time vector for simulation
+t = 0:0.01:150;
+
+% Define input as zero for the short period motion (no external input)
+u = zeros(length(t), 2); % Two input channels for elevator and throttle 
+u(:, 1) = -1; % Negative elevator step input          
+
+% Simulate the response of the system using lsim
+[y_ph, t_out, x_ph] = lsim(sys_long_full, u, t, ph_init_cond);
+%y_ph = step(sys_long_full)
+
+% Plot V_t (Speed)
+figure;
+plot(t_out, x_ph(:, 1), 'b', 'LineWidth', 1.5);
+title('Phugoid Time Response: V_t (Speed)');
+xlabel('Time (s)');
+ylabel('V_t');
+grid on;
+
+% Plot \alpha (Angle of Attack)
+figure;
+plot(t_out, x_ph(:, 2), 'r', 'LineWidth', 1.5);
+title('Phugoid Time Response: \alpha (Angle of Attack)');
+xlabel('Time (s)');
+ylabel('\alpha (deg)');
+grid on;
+
+% Plot \theta (Pitch Angle)
+figure;
+plot(t_out, x_ph(:, 3), 'g', 'LineWidth', 1.5);
+title('Phugoid Time Response: \theta (Pitch Angle)');
+xlabel('Time (s)');
+ylabel('\theta (deg)');
+grid on;
+
+% Plot q (Pitch Rate)
+figure;
+plot(t_out, x_ph(:, 4), 'm', 'LineWidth', 1.5);
+title('Phugoid Time Response: q (Pitch Rate)');
+xlabel('Time (s)');
+ylabel('q (deg/s)');
+grid on;
+
+%%%%%%%%%%%%%%%%%%%%%%%
+%% Lateral Eigenmotions
+%%%%%%%%%%%%%%%%%%%%%%%
+
+C_ac_lat = C_lat([1 2], [1 2 3 4]);
+D_ac_lat = zeros(2, 2);
+
+% Full longitudinal state-space model (4 states: Vt, alpha, theta, q)
+sys_lat_full = ss(A_ac_lat, B_ac_lat, C_ac_lat, D_ac_lat);
+
+% Get the eigenvector corresponding to the short-period mode
+[lat_eigvecs, ~] = eig(A_ac_lat);
+%[num_states, ~] = size(sys_lat_full.A);
+%disp(['Number of states: ', num2str(num_states)]);
+
+
+%%%%%%%%%%%%%
+%% Dutch Roll
+%%%%%%%%%%%%%
+dr_init_cond = real(lat_eigvecs(:, 1));
+%dr_init_cond = [0,0,0,0];
+
+% Create the time vector for simulation
+t = 0:0.01:20;
+
+% Define input as zero for the short period motion (no external input)
+u = zeros(length(t), 2); % Two input channels for aileron and rudder
+%u(:, 1) = 0.01;
+u(:, 2) = -0.01; % Negative rudder step input
+
+% Simulate the response of the system using lsim
+[y_dr, t_out, x_dr] = lsim(sys_lat_full, u, t, dr_init_cond);
+
+% Plot \beta
+figure;
+plot(t_out, x_dr(:, 1), 'b', 'LineWidth', 1.5);
+title('Dutch Roll Time Response: \beta ');
+xlabel('Time (s)');
+ylabel('\beta (deg)');
+grid on;
+
+% Plot \phi 
+figure;
+plot(t_out, x_dr(:, 2), 'r', 'LineWidth', 1.5);
+title('Dutch Roll Time Response: \phi');
+xlabel('Time (s)');
+ylabel('\phi (deg)');
+grid on;
+
+% Plot p (Roll Rate)
+figure;
+plot(t_out, x_dr(:, 3), 'g', 'LineWidth', 1.5);
+title('Dutch Roll Time Response: p (Roll rate)');
+xlabel('Time (s)');
+ylabel('p (deg/s)');
+grid on;
+
+% Plot r (Yaw Rate)
+figure;
+plot(t_out, x_dr(:, 4), 'm', 'LineWidth', 1.5);
+title('Dutch Roll Time Response: r (Yaw Rate)');
+xlabel('Time (s)');
+ylabel('r (deg/s)');
+grid on;
+
+%%%%%%%%%%%%%
+%% A-periodic
+%%%%%%%%%%%%%
+
+
+ar_init_cond = lat_eigvecs(:, 3);
+%ar_init_cond = [0,0,0,0];
+
+% Create the time vector for simulation
+t = 0:0.01:100;
+
+% Define input as zero for the short period motion (no external input)
+u = zeros(length(t), 2); % Two input channels for aileron and rudder
+u(:, 1) = 1; % Positive aileron step input
+%u(:, 2) = -0.01; % Negative rudder step input
+
+% Simulate the response of the system using lsim
+[y_ar, t_out, x_ar] = lsim(sys_lat_full, u, t, ar_init_cond);
+
+% Plot \beta
+figure;
+plot(t_out, x_ar(:, 1), 'b', 'LineWidth', 1.5);
+title('A-periodic Time Response: \beta ');
+xlabel('Time (s)');
+ylabel('\beta (deg)');
+grid on;
+
+% Plot \phi 
+figure;
+plot(t_out, x_ar(:, 2), 'r', 'LineWidth', 1.5);
+title('A-periodic Time Response: \phi');
+xlabel('Time (s)');
+ylabel('\phi (deg)');
+grid on;
+
+% Plot p (Roll Rate)
+figure;
+plot(t_out, x_ar(:, 3), 'g', 'LineWidth', 1.5);
+title('A-periodic Time Response: p (Roll rate)');
+xlabel('Time (s)');
+ylabel('p (deg/s)');
+grid on;
+
+% Plot r (Yaw Rate)
+figure;
+plot(t_out, x_ar(:, 4), 'm', 'LineWidth', 1.5);
+title('A-periodic Time Response: r (Yaw Rate)');
+xlabel('Time (s)');
+ylabel('r (deg/s)');
+grid on;
+
+
+%%%%%%%%%%%%%
+%% Spiral
+%%%%%%%%%%%%%
+
+spiral_init_cond = lat_eigvecs(:, 4);
+%dr_init_cond = [0,0,0,0];
+
+% Create the time vector for simulation
+t = 0:0.01:50;
+
+% Define input as zero for the short period motion (no external input)
+u = zeros(length(t), 2); % Two input channels for aileron and rudder
+u(:, 1) = 1; % Negative aileron step input
+%u(:, 2) = -0.01; % Negative rudder step input
+
+% Simulate the response of the system using lsim
+[y_spiral, t_out, x_spiral] = lsim(sys_lat_full, u, t, spiral_init_cond);
+
+% Plot \beta
+figure;
+plot(t_out, x_spiral(:, 1), 'b', 'LineWidth', 1.5);
+title('Spiral Time Response: \beta ');
+xlabel('Time (s)');
+ylabel('\beta (deg)');
+grid on;
+
+% Plot \phi 
+figure;
+plot(t_out, x_spiral(:, 2), 'r', 'LineWidth', 1.5);
+title('Spiral Time Response: \phi');
+xlabel('Time (s)');
+ylabel('\phi (deg)');
+grid on;
+
+% Plot p (Roll Rate)
+figure;
+plot(t_out, x_spiral(:, 3), 'g', 'LineWidth', 1.5);
+title('Spiral Time Response: p (Roll rate)');
+xlabel('Time (s)');
+ylabel('p (deg/s)');
+grid on;
+
+% Plot r (Yaw Rate)
+figure;
+plot(t_out, x_spiral(:, 4), 'm', 'LineWidth', 1.5);
+title('Spiral Time Response: r (Yaw Rate)');
+xlabel('Time (s)');
+ylabel('r (deg/s)');
+grid on;
 
 
 
